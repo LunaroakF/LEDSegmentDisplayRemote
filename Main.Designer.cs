c80a796b6c46c41177c60b8fb7cda70bc1620d52
LunaroakF <smallkay063@outlook.com>
2023-10-29 19:22:19 +0800

    界面设计 图纸

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.PortBox = new System.Windows.Forms.TextBox();
@@ -59,7 +60,7 @@
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(228, 84);
+            this.groupBox1.Size = new System.Drawing.Size(309, 105);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "远程地址";
@@ -67,17 +68,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 52);
+            this.label2.Location = new System.Drawing.Point(8, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.Size = new System.Drawing.Size(54, 28);
             this.label2.TabIndex = 3;
             this.label2.Text = "端口";
             // 
             // PortBox
             // 
-            this.PortBox.Location = new System.Drawing.Point(45, 49);
+            this.PortBox.Location = new System.Drawing.Point(117, 64);
             this.PortBox.Name = "PortBox";
-            this.PortBox.Size = new System.Drawing.Size(176, 23);
+            this.PortBox.Size = new System.Drawing.Size(176, 35);
             this.PortBox.TabIndex = 2;
             this.PortBox.Text = "2000";
             this.PortBox.TextChanged += new System.EventHandler(this.PortBox_TextChanged);
@@ -88,15 +89,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(7, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.Size = new System.Drawing.Size(31, 28);
             this.label1.TabIndex = 1;
             this.label1.Text = "IP";
             // 
             // IPBox
             // 
-            this.IPBox.Location = new System.Drawing.Point(45, 23);
+            this.IPBox.Location = new System.Drawing.Point(117, 19);
             this.IPBox.Name = "IPBox";
-            this.IPBox.Size = new System.Drawing.Size(176, 23);
+            this.IPBox.Size = new System.Drawing.Size(176, 35);
             this.IPBox.TabIndex = 900;
             this.IPBox.Text = "192.168.0.121";
             this.IPBox.TextChanged += new System.EventHandler(this.IPBox_TextChanged);
@@ -108,20 +109,20 @@
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.CommandBox);
-            this.groupBox2.Location = new System.Drawing.Point(4, 95);
+            this.groupBox2.Location = new System.Drawing.Point(4, 116);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(228, 141);
+            this.groupBox2.Size = new System.Drawing.Size(309, 195);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "自定信息";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(154, 106);
+            this.button1.Location = new System.Drawing.Point(197, 149);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 27);
+            this.button1.Size = new System.Drawing.Size(105, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "发送";
             this.button1.UseVisualStyleBackColor = true;
@@ -130,35 +131,35 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(29, 54);
+            this.label5.Location = new System.Drawing.Point(21, 84);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(162, 17);
+            this.label5.Size = new System.Drawing.Size(281, 28);
             this.label5.TabIndex = 6;
             this.label5.Text = "后四为小数点是否亮起(0或1)";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(51, 37);
+            this.label4.Location = new System.Drawing.Point(59, 56);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(116, 17);
+            this.label4.Size = new System.Drawing.Size(201, 28);
             this.label4.TabIndex = 5;
             this.label4.Text = "前四数字为显示数字";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(47, 20);
+            this.label3.Location = new System.Drawing.Point(47, 28);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.Size = new System.Drawing.Size(213, 28);
             this.label3.TabIndex = 4;
             this.label3.Text = "格式1-2-3-4-0-1-0-1";
             // 
             // CommandBox
             // 
-            this.CommandBox.Location = new System.Drawing.Point(7, 77);
+            this.CommandBox.Location = new System.Drawing.Point(7, 113);
             this.CommandBox.Name = "CommandBox";
-            this.CommandBox.Size = new System.Drawing.Size(211, 23);
+            this.CommandBox.Size = new System.Drawing.Size(295, 35);
             this.CommandBox.TabIndex = 0;
             this.CommandBox.Text = "1-2-3-4-0-1-0-1";
             // 
@@ -168,9 +169,9 @@
             this.groupBox3.Controls.Add(this.AutoStop);
             this.groupBox3.Controls.Add(this.AutoStart);
             this.groupBox3.Controls.Add(this.comboBox1);
-            this.groupBox3.Location = new System.Drawing.Point(242, 95);
+            this.groupBox3.Location = new System.Drawing.Point(320, 130);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(242, 140);
+            this.groupBox3.Size = new System.Drawing.Size(332, 181);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "自动操作";
@@ -180,9 +181,9 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(6, 53);
+            this.checkBox1.Location = new System.Drawing.Point(80, 76);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(106, 21);
+            this.checkBox1.Size = new System.Drawing.Size(176, 32);
             this.checkBox1.TabIndex = 9;
             this.checkBox1.Text = "自动填充数字0";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -192,9 +193,9 @@
             // 
             this.AutoStop.Enabled = false;
             this.AutoStop.ForeColor = System.Drawing.Color.Red;
-            this.AutoStop.Location = new System.Drawing.Point(6, 107);
+            this.AutoStop.Location = new System.Drawing.Point(6, 134);
             this.AutoStop.Name = "AutoStop";
-            this.AutoStop.Size = new System.Drawing.Size(67, 27);
+            this.AutoStop.Size = new System.Drawing.Size(105, 41);
             this.AutoStop.TabIndex = 8;
             this.AutoStop.Text = "终止";
             this.AutoStop.UseVisualStyleBackColor = true;
@@ -202,9 +203,9 @@
             // 
             // AutoStart
             // 
-            this.AutoStart.Location = new System.Drawing.Point(167, 105);
+            this.AutoStart.Location = new System.Drawing.Point(221, 134);
             this.AutoStart.Name = "AutoStart";
-            this.AutoStart.Size = new System.Drawing.Size(67, 27);
+            this.AutoStart.Size = new System.Drawing.Size(105, 41);
             this.AutoStart.TabIndex = 7;
             this.AutoStart.Text = "开始";
             this.AutoStart.UseVisualStyleBackColor = true;
@@ -222,22 +223,23 @@
             "GPU内存MB",
             "系统时间",
             "下行网速MB/s"});
-            this.comboBox1.Location = new System.Drawing.Point(6, 22);
+            this.comboBox1.Location = new System.Drawing.Point(6, 34);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(139, 25);
+            this.comboBox1.Size = new System.Drawing.Size(320, 36);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.ValueMember = "1";
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 239);
+            this.ClientSize = new System.Drawing.Size(664, 323);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Main";
